fadabda1927a2ea6b24c608ed2b93d72f586f5f4
Amitchauhangit <39885098+Amitchauhangit@users.noreply.github.com>
2019-06-30 15:51:52 +0530

    Update configure.py

--- configure.py
+++ configure.py
@@ -6,11 +6,13 @@ Created on Wed Nov 14 01:28:57 2018
 """
 
 from subprocess import Popen,PIPE
-p=Popen(["aws","configure"],stdin=PIPE)
-p.communicate("AKIAJNVX2MRBHIBSXOMQ")
-p.communicate("#\n")
-p.communicate("smXgfQ2b4Lw8O51RmDLh+wYepWg1oaYWhfQpa8PB")
-p.communicate("#\n")
-p.communicate("us-east-2")
-p.communicate("#\n")
+
+def config():
+      p=Popen(["aws","configure"],stdin=PIPE)
+      p.communicate("AKIAJNVX2MRBHIBSXOMQ")
+      p.communicate("#\n")
+      p.communicate("smXgfQ2b4Lw8O51RmDLh+wYepWg1oaYWhfQpa8PB")
+      p.communicate("#\n")
+      p.communicate("us-east-2")
+      p.communicate("#\n")
 
